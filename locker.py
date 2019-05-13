b974051b11121f214150754728968e5d347ff249
xystus ngigi <xystusngigi@gmail.com>
2019-05-13 11:10:58 +0300

    isnertions made

--- locker.py
+++ locker.py
@@ -99,6 +99,5 @@ class Credential:
         '''
         Class method that copies a credential's info after the credential's site name is entered
         '''
-        find_credential = Credential.find_by_site_name(site_name)
-        
+        find_credential = Credential.find_by_site_name(site_name)        
         return pyperclip.copy(find_credential.password)
